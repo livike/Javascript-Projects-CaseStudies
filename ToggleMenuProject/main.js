d3b403aa5595a71087a2f4f2d10200ecfbdc7681
Dobai Lívia <livia.dobai@gmail.com>
2020-01-27 17:48:24 +0100

    Added the toggle functionality with javascript

--- ToggleMenuProject/main.js
+++ ToggleMenuProject/main.js
@@ -1,1 +1,33 @@
-le
+let toggleNavStatus = false;
+
+let toggleNav = function() {
+  let getSidebar = document.querySelector('.nav-sidebar');
+  let getSidebarUl = document.querySelector('.nav-sidebar ul');
+  let getSidebarTitle = document.querySelector('.nav-sidebar span');
+  let getSidebarLinks = document.querySelectorAll('.nav-sidebar a');
+
+  if(toggleNavStatus === false) {
+    getSidebarUl.style.visibility = 'visible';
+    getSidebar.style.width = '272px';
+    getSidebarTitle.style.opacity = '0.5';
+
+    let arrayLength = getSidebarLinks.length;
+    for(var i=0; i<arrayLength; i++) {
+      getSidebarLinks[i].style.opacity = 1;
+    }
+    toggleNavStatus = true;
+  } 
+  else if(toggleNavStatus === true) {
+    
+    getSidebar.style.width = '50px';
+    getSidebarTitle.style.opacity = '0';
+
+    let arrayLength = getSidebarLinks.length;
+    for(var i=0; i<arrayLength; i++) {
+      getSidebarLinks[i].style.opacity = 1;
+    }
+    getSidebarUl.style.visibility = 'hidden';
+    toggleNavStatus = false;
+  }
+
+};
